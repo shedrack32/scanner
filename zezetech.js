8318c89ea5ad63a641c6c0461a5e440034e97dba
Humphrey47Bot MD <humphreymbise68@gmail.com>
2025-05-15 14:36:51 +0300

    Update and rename Zezetech.js to zezetech.js

--- zezetech.js
+++ zezetech.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Topu-Qr-Scanner is Live
+Zeze-Qr-Scanner is Live
 
  Server running on http://localhost:` + PORT)
 })
